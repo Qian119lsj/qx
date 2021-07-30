f4b781876db43ac8ad25e774797fab10b8981e17
Qian119lsj <43394347+Qian119lsj@users.noreply.github.com>
2021-07-30 15:25:40 +0800

    Update qx.js

--- qx.js
+++ qx.js
@@ -1,49 +1,19 @@
-/*
-酷我音乐 解锁会员试听及部分功能
+/**
+ * @fileoverview Example of HTTP rewrite of request header.
+ *
+ * @supported Quantumult X (v1.0.5-build188)
+ *
+ * [rewrite_local]
+ * ^http://example\.com/resource9/ url script-request-header sample-rewrite-request-header.js
+ */
 
-***************************
-QuantumultX:
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
 
-[rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+var modifiedHeaders = $request.headers;
+modifiedHeaders['X-T5-Auth'] = '88888888';
 
-[mitm]
-hostname = vip1.kuwo.cn
+// var modifiedPath = '/api2/abc?k=v';
 
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
-
-[MITM]
-hostname = vip1.kuwo.cn
-
-**************************/
-
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/vip/v2/user/vip';
-const time = '/vip/spi/mservice';
-
-if (url.indexOf(vip) != -1) {
-	obj.data["isNewUser"] = "2";
-	obj.data["vipLuxuryExpire"] = "1835312949000";
-	obj.data["time"] = "1961170340993";
-	obj.data["isYearUser"] = "2";
-	obj.data["vipmExpire"] = "1835312949000";
-	obj.data["vipOverSeasExpire"] = "1835312949000";
-	obj.data["vipExpire"] = "1835312949000";
-	obj.data["vip3Expire"] = "1835312949000";
-	body = JSON.stringify(obj);
-}
-
-if (url.indexOf(time) != -1) {
-	obj["isVIPMAutoPay"] = 2;
-	obj["isVIPLuxAutoPay"] = 2;
-	body = JSON.stringify(obj);
-}
-
-$done({body});
+$done({headers : modifiedHeaders});
+// $done({path : modifiedPath});
+// $done({}); // Not changed.
