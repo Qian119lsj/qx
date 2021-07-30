c1122e93b49b3ab9d04a3c80ea21881438c04e50
Qian119lsj <43394347+Qian119lsj@users.noreply.github.com>
2021-07-30 17:57:19 +0800

    Update qx1.js

--- qx1.js
+++ qx1.js
@@ -4,8 +4,10 @@ function Change_Request()
     delete RequestHeaders.Connection;
     RequestHeaders['Proxy-Connection'] = 'Keep-Alive';
     RequestHeaders['X-T5-Auth'] = '88888888';
-    $notification.post("p","t1",JSON.stringify($request.headers['X-T5-Auth']))
+    console.log($request.headers);
+//     $notification.post("p","t1",JSON.stringify($request.headers['X-T5-Auth']))
     $done({headers:RequestHeaders});//修改完成之后需要调用$done
-    $notification.post("p","t2",JSON.stringify($request.headers['X-T5-Auth']))
+    console.log($request.headers);
+//     $notification.post("p","t2",JSON.stringify($request.headers['X-T5-Auth']))
 }
 Change_Request();
