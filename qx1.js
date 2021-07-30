8d61e1434d18fbcde88afa678ddd4f9307bfef79
Qian119lsj <43394347+Qian119lsj@users.noreply.github.com>
2021-07-30 17:05:14 +0800

    Update qx1.js

--- qx1.js
+++ qx1.js
@@ -1,7 +1,10 @@
-//修改请求头信息
-
+console.log('================11');
+function Change_Request()
+{
+    console.log('================22');
     var RequestHeaders = $request.headers;
     delete RequestHeaders.Connection;
     RequestHeaders['Proxy-Connection'] = 'Keep-Alive';
     RequestHeaders['X-T5-Auth'] = '88888888';
     $done({RequestHeaders});//修改完成之后需要调用$done
+}
