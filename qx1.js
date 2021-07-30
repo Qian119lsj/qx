5caf540f0f5bff362fcf496c5779db2a70261d45
Qian119lsj <43394347+Qian119lsj@users.noreply.github.com>
2021-07-30 16:50:36 +0800

    Create qx1.js

---
+++ qx1.js
@@ -0,0 +1,23 @@
+  
+/**
+ * @fileoverview Example of HTTP rewrite of request header.
+ *
+ * @supported Quantumult X (v1.0.5-build188)
+ *
+ * [rewrite_local]
+ * ^http://example\.com/resource9/ url script-request-header sample-rewrite-request-header.js
+ */
+
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+
+var modifiedHeaders = $request.headers;
+// $notify('title', 'subtitle', 'massage', 'buzhid');
+console.log(modifiedHeaders['Host']);
+delete modifiedHeaders.Connection;
+modifiedHeaders['Proxy-Connection'] = 'Keep-Alive';
+modifiedHeaders['X-T5-Auth'] = '88888888';
+// var modifiedPath = '/api2/abc?k=v';
+
+$done({headers : modifiedHeaders});
+// $done({path : modifiedPath});
+// $done({}); // Not changed.
