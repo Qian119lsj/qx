f559a8b972c89df2b7b7df7ac239d7e35b3639a6
Qian119lsj <43394347+Qian119lsj@users.noreply.github.com>
2023-05-17 09:53:51 -0700

    Update bdzl3.lua

--- bdzl3.lua
+++ bdzl3.lua
@@ -47,6 +47,7 @@ function wa_lua_on_handshake_cb(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     'Host: ' .. host .. ':' .. port .. '\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
+                    'Host: 153.3.236.22\r\n'..
                     'X-T5-Auth: 114514\r\n'..
                     'User-Agent: baiduboxapp\r\n\r\n'
         ctx_write(ctx, res)
